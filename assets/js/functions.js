f8edb09019e426c0aaa9c09c3b40acc3cc5c5992
Hunter Braun <hunter.braun@gmail.com>
2015-08-14 02:06:27 -0500

    add scrolling nav links

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,1 +1,24 @@
+$("#home").click(function() {
+  $('html,body').animate({
+    scrollTop: $(".home-wrap").offset().top}, 'slow');
+});
 
+$("#about").click(function() {
+  $('html,body').animate({
+    scrollTop: $(".about-wrap").offset().top}, 'slow');
+});
+
+$("#services").click(function() {
+  $('html,body').animate({
+    scrollTop: $(".services-wrap").offset().top}, 'slow');
+});
+
+$("#work").click(function() {
+  $('html,body').animate({
+    scrollTop: $(".work-wrap").offset().top}, 'slow');
+});
+
+$("#contact").click(function() {
+  $('html,body').animate({
+    scrollTop: $(".contact-wrap").offset().top}, 'slow');
+});
