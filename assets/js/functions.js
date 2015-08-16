6e403b4be780e68f7375db3e4dda0809c06b5a50
Hunter Braun <hunter.braun@gmail.com>
2015-08-16 16:58:03 -0500

    add image gallery

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,15 @@
+$(document).ready(function() {
+  $('.image-link').magnificPopup({type:'image'});
+});
+
+// This will create a single gallery from all elements that have class "gallery-item"
+$('.gallery-item').magnificPopup({
+  type: 'image',
+  gallery:{
+    enabled:true
+  }
+});
+
 $('#home, #about, #services, #work, #contact').click(function() {
   var id = $(this).attr('id');
 
